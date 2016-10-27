e62ff7076be40fc50fb157ed61581d62927bda3e
Raim Interiano <rai.interiano@gmail.com>
2016-10-26 23:14:20 -0600

    Word class created; gives the word, length, and can check if it contains the letter that you are looking for

--- src/solidhangman/game/composers/Word.java
+++ src/solidhangman/game/composers/Word.java
@@ -1,8 +1,23 @@
 package solidhangman.game.composers;
 
-class Word {
-
+public class Word {
+    private String wordText;
+    private int wordLength;
+    
     public Word(String word) {
+        this.wordText = word;
+        this.wordLength = word.length();
+    }
+  
+    public String getWordText() {
+        return wordText;
     }
     
+    public int getWordLength() {
+        return wordLength;
+    }
+    
+    public int containsLetter(char letter){
+        return this.wordText.indexOf(letter);
+    }
 }
